ba639fa2ce1de608f84262ada390a660c185e78f
captainbot <liuyu9045@gmail.com>
2026-01-17 04:13:41 +0800

    add anima

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-lottie.loadAnimation({
+const anim = lottie.loadAnimation({
   container: document.getElementById('bm'),
   renderer: 'svg',
   loop: true,
@@ -6,4 +6,8 @@ lottie.loadAnimation({
   path: 'data.json'
 });
 
-document.getElementsByTagName('body')[0].classList.add('show');
+anim.addEventListener('DOMLoaded', () => {
+  console.log('动画播放结束！');
+  // 在此处执行后续逻辑，如跳转页面或隐藏容器
+  document.getElementsByTagName('body')[0].classList.add('show');
+});
